bff40774ea2b56245bb56ef24af5e7c50f6838fe
Mikhail <pmp@nopreset.ru>
2020-07-07 00:13:50 -0400

    Perfect Initial Setup

---
+++ src/client/react/pages/home/index.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three'
+
+import React, { Component, useCallback, useEffect, useState, useRef, useMemo } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+const REACT_VERSION = React.version;
+
+
+class HomePage extends Component {
+
+	render() {
+
+		return (
+     		<div>
+				  This is home. Updated
+				  <Link to="/about"> Go to about > </Link>
+				  React version: {REACT_VERSION}
+			</div>
+		);
+	}
+}
+
+function mapStateToProps({ app }) {
+	return {
+	};
+}
+
+export default {
+	component: connect(mapStateToProps, {})(HomePage)
+}
