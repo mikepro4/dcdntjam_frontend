5096bdd1339ade027845dfab0fa037da861c43aa
Mikhail <pmp@nopreset.ru>
2020-07-08 00:44:19 -0400

    fixed animation of menu

--- src/client/react/components/header/index.js
+++ src/client/react/components/header/index.js
@@ -3,9 +3,15 @@ import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
 import classNames from "classnames"
 import posed, { PoseGroup } from 'react-pose';
+import Logo from '../icons/logo'
+
+import Anime from 'react-anime';
 
 class Header extends Component {
 
+	componentDidMount() {
+
+	}
 	renderAuthButton() {
 		return this.props.user.profile ? (
 			<div className="user-info">
@@ -22,9 +28,11 @@ class Header extends Component {
 				</Link> */}
 			</div>
 
-			<a href="/api/logout" className="logout-button">
+		
+
+			{/* <a href="/api/logout" className="logout-button">
 				Logout
-			</a>
+			</a> */}
 
 			</div>
 		) : (
@@ -37,11 +45,77 @@ class Header extends Component {
 	}
 
 	render() {
+
+		const d = [
+				{ value: 'M39.50625,9.5 C30.8788547,9.5 28.639837,0.5 20,0.5 C11.360163,0.5 8.88972652,9.5 0.5,9.5' }]
 		return (
 			<div className="app-header">
 
         	<div className="header-left">
+				<Logo/>
+			</div>
+			<div className="header-right">
 				{this.renderAuthButton()}
+			</div>
+
+			<div className="menu_icon">
+				
+				<div className="line_bottom">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="40"
+						height="10"
+					>
+						<g
+						fill="none"
+						fillRule="evenodd"
+						stroke="#FFF"
+						strokeLinecap="square"
+						strokeWidth="1"
+						>
+						<Anime
+							easing="easeInOutCubic"
+							duration={1000}
+							d={d}
+							direction='alternate'
+							loop={true}
+							key={11+Date.now()}
+						>
+								<path d="M39.50625,9.5 C30.8788547,9.5 28.642962,9.5 20.003125,9.5 C11.363288,9.5 8.88972652,9.5 0.5,9.5"></path>
+						</Anime>
+						
+						</g>
+					</svg>
+
+				</div>
+
+				<div className="line_top">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="40"
+						height="10"
+					>
+						<g
+						fill="none"
+						fillRule="evenodd"
+						stroke="#FFF"
+						strokeLinecap="square"
+						strokeWidth="1"
+						>
+						<Anime
+							easing="easeInOutCubic"
+							duration={1000}
+							d={[{value: "M39.50625,0.5 C30.8788547,0.5 28.642962,9.5109931 20.003125,9.5109931 C11.363288,9.5109931 8.88972652,0.5 0.5,0.5"}]}
+							direction='alternate'
+							loop={true}
+							key={11+Date.now()}
+						>
+								<path d="M39.50625,0.5 C30.8788547,0.5 28.642962,0.5 20.003125,0.5 C11.363288,0.5 8.88972652,0.5 0.5,0.5"></path>
+						</Anime>
+						
+						</g>
+					</svg>
+				</div>
 			</div>
       </div>
 		);
